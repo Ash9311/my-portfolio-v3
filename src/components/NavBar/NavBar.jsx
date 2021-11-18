71b6e9d988a69a0e6ed3db8b6b455967403628cf
Ashutosh Mulky <ashutoshmulki7@gmail.com>
2021-11-18 23:48:22 +0530

    nav styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,68 @@
-import React from "react";
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import { Link, withRouter } from 'react-router-dom';
+
 import './NavBar.css';
-import {slide as Menu} from 'react-burger-menu';
-import {Link,withRouter} from 'react-router-dom';
 
 class NavBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            menuOpen:false
+        };
+    }
     
+    handleStateChange = (state) => {
+        this.setState({
+            menuOpen: state.isOpen
+        })
+    }
+
+    closeMenu = () => {
+        this.setState({
+            menuOpen: false
+        })
+    }
 
     render() {
-        
-        // const {location} = this.props;
-        const {location} = this.props;
-   
-        const homeClass = location.pathname === '/' ? 'active-item': '';
-        const aboutClass = location.pathname === '/about' ? 'active-item': '';
-        const projectsClass = location.pathname === '/projects' ? 'active-item': '';
-        const skillsClass = location.pathname === '/skills' ? 'active-item': '';
-        const contactClass = location.pathname === '/contact' ? 'active-item': '';
-        return ( 
-            <Menu>
-                {/* we never want to use <a> tag inside react router application */}
-                {/* <a id="home" className="menu-item" href="/">Home</a> */}
-                <Link to="/" className={`menu-item ${homeClass}`}>Home</Link>
-                <Link to="/about" className={`menu-item ${aboutClass}`}>About</Link>
-                <Link to="/projects" className={`menu-item ${projectsClass}`}>Projects</Link>
-                <Link to="/skills" className={`menu-item ${skillsClass}`}>Skills</Link>
-                <Link to="/contact" className={`menu-item ${contactClass}`}>Contact</Link>
-               
+        const { location } = this.props;
+
+        const homeClass = location.pathname === '/' ? 'active-item' : '';
+        const aboutClass = location.pathname === '/about' ? 'active-item' : '';
+        const projectsClass =
+            location.pathname === '/projects' ? 'active-item' : '';
+        const skillsClass =
+            location.pathname === '/skills' ? 'active-item' : '';
+        const contactClass =
+            location.pathname === '/contact' ? 'active-item' : '';
+
+        return (
+            <Menu 
+                isOpen={this.state.menuOpen}
+                onStateChange={(state)=>this.handleStateChange(state)}
+            >
+                {/* We NEVER want to use <a> inside our react router application */}
+                {/* <a id='home' className='menu-item' href='/'>
+                    Home
+                </a> */}
+                <Link to='/' onClick ={()=>this.closeMenu()} className={`menu-item ${homeClass}`}>
+                    Home
+                </Link>
+                <Link to='/about' onClick ={()=>this.closeMenu()} className={`menu-item ${aboutClass}`}>
+                    About
+                </Link>
+                <Link to='/projects' onClick ={()=>this.closeMenu()} className={`menu-item ${projectsClass}`}>
+                    Projects
+                </Link>
+                <Link to='/skills' onClick ={()=>this.closeMenu()} className={`menu-item ${skillsClass}`}>
+                    Skills
+                </Link>
+                <Link to='/contact' onClick ={()=>this.closeMenu()} className={`menu-item ${contactClass}`}>
+                    Contact
+                </Link>
             </Menu>
         );
     }
 }
+
 export default withRouter(NavBar);
