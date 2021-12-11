7dfb5ab9f315bf0193657600b09dd53d32c0a4c7
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2021-12-11 16:18:40 +0530

    Add files via upload

---
+++ src/assets/projectsData.js
@@ -0,0 +1,38 @@
+export const projectsData = [
+    {
+        projectName: 'Blog-Site',
+        projectDescription:
+            'A blogging page where the user can post his/her blogs and perform several operations. designed using HTML, CSS, javaScript followed by nodeJs,expressJs.',
+        imageUrl: 'https://github.com/Ash9311/My_Portfolio/blob/main/assets/blog-site.png?raw=true',
+        videoUrl:
+            '',
+        projectUrl: 'https://github.com/Ash9311/Blog-site',
+    },
+    {
+        projectName: 'Weather-app',
+        projectDescription:
+            'A Google Maps & Weather API based application for realtime weather information on Maps with additional functionalities.',
+        imageUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/Weather%20project.png?raw=true',
+        videoUrl:
+            '',
+        projectUrl: 'https://github.com/Ash9311/Weather-Project',
+    },
+    {
+        projectName: 'ToDo-List',
+        projectDescription:
+            'Having problems organizing your daily schedule? No problem ! we have a ToDo List where you can add your daily tasks and delete once done.',
+        imageUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/To-do-List.png?raw=true',
+        videoUrl:
+            '',
+        projectUrl: 'https://github.com/Ash9311/ToDo-List',
+    },
+   
+    {
+        projectName: 'Portfolio Website',
+        projectDescription:
+            'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. Check out my portfolio here',
+        imageUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/homepageimage.PNG?raw=true',
+        videoUrl: '',
+        projectUrl: 'https://github.com/Ash9311/my-portfolio-v2',
+    },
+];
---
+++ src/assets/skillsData.js
@@ -0,0 +1,58 @@
+export const skillList = [
+    {
+        skillName: 'HTML',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/html.png?raw=true',
+    },
+    {
+        skillName: 'CSS',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/css.png?raw=true',
+    },
+    {
+        skillName: 'JavaScript',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/js.png?raw=true',
+    },
+    {
+        skillName: 'Angular',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/42a91198a5438f1ce70401a45fb6f907bbd8868b/assets/angular.png?raw=true',
+    },
+    {
+        skillName: 'React',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/react.png?raw=true',
+    },
+  
+    {
+        skillName: 'NodeJS',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/node.png?raw=true',
+    },
+    {
+        skillName: 'TypeScript',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/ts.png?raw=true',
+    },
+    {
+        skillName: 'Python',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/python.png?raw=true',
+
+    },
+    {
+        skillName: 'Java',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/java.png?raw=true',
+    },
+    {
+        skillName: 'C++',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/C++.png?raw=true',
+    },
+   
+    {
+        skillName: 'C#',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/cs.png?raw=true',
+    },
+    {
+        skillName: 'MongoDB',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/mongo.png?raw=true',
+    },
+    {
+        skillName: 'SQL',
+        skillUrl: 'https://github.com/Ash9311/My_Portfolio-assets/blob/main/assets/SQL.png?raw=true',
+    },
+    
+];
